43c4e39e11bc88cd94c48bc7341ca21210c02f4f
Muaadahmed <muaad.abdi.ahmed@gmail.com>
2022-04-14 08:13:22 -0400

    Add PUT request, clean up code, and test api

--- routes/recipes.js
+++ routes/recipes.js
@@ -18,18 +18,39 @@ router.get('/details/:name', (req, res) => {
 router.post('/', (req, res) => {
 
   for (let recipe of recipesData.recipes) {
-    console.log("recipe....", recipesData.recipes[recipesData.recipes.indexOf(recipe)].name);
     if (recipesData.recipes[recipesData.recipes.indexOf(recipe)].name === req.body.name) {
       return res.json({ error: "Recipe already exists"}).status(400);
     }
   }
 
   recipesData.recipes.push(req.body);
-  console.log(recipesData.recipes);
   
-  console.log('POST ROUTE REACHED');
-  res.send("POST ROUTE REACHED");
-  console.log(req.body);
+  res.send({}).status(201);
+});
+
+router.put('/:name', (req, res) => {
+  const {name} = req.params;
+  const {ingredients, instructions} = req.body; 
+
+  const recipe = recipesData.recipes.find((recipe) => recipe.name === name);
+  
+  if (recipe === undefined) {
+    return res.send({error: "Recipe does not exist"}).status(404);
+  }
+
+  if (ingredients) {
+    recipe.ingredients = ingredients;
+    console.log("recipe ingredient changes ", recipe.ingredients)
+  }
+
+  if (instructions) {
+    recipe.instructions = instructions;
+    console.log("recipe instructions changes ", recipe.instructions)
+  }
+
+  recipesData.recipes[recipesData.recipes.indexOf(recipe)] = recipe;
+  console.log("recipe Data MOD: ", recipesData.recipes);
+  res.send({}).status(204);
 });
 
 module.exports = router;
